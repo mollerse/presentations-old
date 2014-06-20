4785b96d1b2bbca92195dccabd5c024b24d79680
Stian Veum Møllersen <stian.moellersen@gmail.com>
2014-06-20 10:30:53 +0200

    Finished ng-bad

--- ng-bad/gulpfile.js
+++ ng-bad/gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var lr = require('gulp-livereload');
-var embedlr = require('gulp-embedlr');
 
 gulp.task('default', function () {
   gulp.watch('index.html').on('change', lr.changed);
